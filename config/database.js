05d85a4faeb28f88a9a63ea0f5ad85fd4a05aeea
MatheusLeitao <matheus_ramos@outlook.com>
2020-07-29 21:31:31 -0300

    Mail package install

--- config/database.js
+++ config/database.js
@@ -16,7 +16,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get('DB_CONNECTION', 'mysql'),
 
   /*
   |--------------------------------------------------------------------------
@@ -52,12 +52,12 @@ module.exports = {
     client: 'mysql',
     connection: {
       host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
+      port: Env.get('DB_PORT', '3306'),
+      user: Env.get('DB_USER', 'node'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      database: Env.get('DB_DATABASE', 'ecommerce')
     },
-    debug: Env.get('DB_DEBUG', false)
+    debug: Env.get('DB_DEBUG', true)
   },
 
   /*
