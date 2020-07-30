9399ce2b1edb31f988ba838988974aa6812e09c6
MatheusLeitao <matheus_ramos@outlook.com>
2020-07-30 10:06:29 -0300

    WebScoket installed, AdonisAcl, Bumblebee and Migrations building

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const BaseExceptionHandler = use('BaseExceptionHandler')
-
+const Logger = use('Logger');
 /**
  * This class handles all exceptions thrown during
  * the HTTP request lifecycle.
@@ -20,7 +20,7 @@ class ExceptionHandler extends BaseExceptionHandler {
    *
    * @return {void}
    */
-  async handle (error, { request, response }) {
+  async handle(error, { request, response }) {
     response.status(error.status).send(error.message)
   }
 
@@ -34,7 +34,15 @@ class ExceptionHandler extends BaseExceptionHandler {
    *
    * @return {void}
    */
-  async report (error, { request }) {
+  async report(error, { request }) {
+    if (error.status >= 500) {
+      Logger.error(error.message, {
+        stack: error.stack,
+        message: error.message,
+        status: error.status,
+        name: error.name
+      })
+    }
   }
 }
 
--- config/app.js
+++ config/app.js
@@ -125,115 +125,30 @@ module.exports = {
     |
     */
     dotfiles: 'ignore',
-
-    /*
-    |--------------------------------------------------------------------------
-    | ETag
-    |--------------------------------------------------------------------------
-    |
-    | Enable or disable etag generation
-    |
-    */
     etag: true,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Extensions
-    |--------------------------------------------------------------------------
-    |
-    | Set file extension fallbacks. When set, if a file is not found, the given
-    | extensions will be added to the file name and search for. The first
-    | that exists will be served. Example: ['html', 'htm'].
-    |
-    */
     extensions: false
   },
 
   locales: {
-    /*
-    |--------------------------------------------------------------------------
-    | Loader
-    |--------------------------------------------------------------------------
-    |
-    | The loader to be used for fetching and updating locales. Below is the
-    | list of available options.
-    |
-    | file, database
-    |
-    */
     loader: 'file',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Default Locale
-    |--------------------------------------------------------------------------
-    |
-    | Default locale to be used by Antl provider. You can always switch drivers
-    | in runtime or use the official Antl middleware to detect the driver
-    | based on HTTP headers/query string.
-    |
-    */
     locale: 'en'
   },
 
   logger: {
-    /*
-    |--------------------------------------------------------------------------
-    | Transport
-    |--------------------------------------------------------------------------
-    |
-    | Transport to be used for logging messages. You can have multiple
-    | transports using same driver.
-    |
-    | Available drivers are: `file` and `console`.
-    |
-    */
-    transport: 'console',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Console Transport
-    |--------------------------------------------------------------------------
-    |
-    | Using `console` driver for logging. This driver writes to `stdout`
-    | and `stderr`
-    |
-    */
+    transport: 'file',
     console: {
       driver: 'console',
       name: 'adonis-app',
       level: 'info'
     },
-
-    /*
-    |--------------------------------------------------------------------------
-    | File Transport
-    |--------------------------------------------------------------------------
-    |
-    | File transport uses file driver and writes log messages for a given
-    | file inside `tmp` directory for your app.
-    |
-    | For a different directory, set an absolute path for the filename.
-    |
-    */
     file: {
       driver: 'file',
-      name: 'adonis-app',
-      filename: 'adonis.log',
+      name: 'ecommerce-realtime',
+      filename: 'ecommerce-error.log',
       level: 'info'
     }
   },
-
-  /*
-  |--------------------------------------------------------------------------
-  | Generic Cookie Options
-  |--------------------------------------------------------------------------
-  |
-  | The following cookie options are generic settings used by AdonisJs to create
-  | cookies. However, some parts of the application like `sessions` can have
-  | separate settings for cookies inside `config/session.js`.
-  |
-  */
   cookie: {
     httpOnly: true,
     sameSite: false,
