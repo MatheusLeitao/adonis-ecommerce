90338a4f9d392d78d6ed6f3d63a2fcd95126f81c
MatheusLeitao <matheus_ramos@outlook.com>
2020-08-05 09:28:47 -0300

    Client Roles and client seeder, applying client roles and creating admin user

---
+++ database/seeds/0001_RoleSeeder.js
@@ -0,0 +1,42 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| RoleSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** !type {import('@adonisjs/lucid/src/Factory')} */
+// const Factory = use('Factory')
+
+const Role = use('Role')
+
+class RoleSeeder {
+  async run() {
+
+    await Role.create({
+      name: 'Admin',
+      slug: 'admin',
+      description: 'System administrator'
+    })
+
+    await Role.create({
+      name: 'Manager',
+      slug: 'manager',
+      description: 'Store manager'
+    })
+
+    await Role.create({
+      name: 'Client',
+      slug: 'client',
+      description: 'Stores client'
+    })
+
+  }
+}
+
+module.exports = RoleSeeder
