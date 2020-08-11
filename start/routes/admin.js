79e2a772ffd3082b7aac4633075b98a68816459b
MatheusLeitao <matheus_ramos@outlook.com>
2020-08-11 19:40:02 -0300

    Admin route and controllers finished

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -4,3 +4,13 @@
 
 const Route = use('Route')
 
+Route.group(() => {
+
+    Route.resource('categories', 'CategoryController').apiOnly()
+    Route.resource('products', 'ProductController').apiOnly()
+    Route.resource('images', 'ImageController').apiOnly()
+    Route.resource('coupons', 'CouponController').apiOnly()
+    Route.resource('users', 'UserController').apiOnly()
+    Route.resource('orders', 'OrderController').apiOnly()
+
+}).prefix('v1/admin').namespace('Admin')
