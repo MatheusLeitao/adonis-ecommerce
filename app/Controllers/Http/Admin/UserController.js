09dc0535799eb395092e7071e59637b21f4ead83
MatheusLeitao <matheus_ramos@outlook.com>
2020-08-28 09:34:10 -0300

    Users controllers done

--- app/Controllers/Http/Admin/UserController.js
+++ app/Controllers/Http/Admin/UserController.js
@@ -4,9 +4,9 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
-/**
- * Resourceful controller for interacting with users
- */
+const Users = use('App/Models/User')
+
+
 class UserController {
   /**
    * Show a list of all users.
@@ -16,20 +16,26 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    * @param {View} ctx.view
+   * @param {object} ctx.pagination
    */
-  async index ({ request, response, view }) {
-  }
 
-  /**
-   * Render a form to be used for creating a new user.
-   * GET users/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async index({ request, response, pagination }) {
+    const name = request.input('name');
+    const surname = request.input('surname');
+    const email = request.input('email');
+    const query = Users.query()
+
+    console.log(name);
+    if (name) {
+      query.where('name', 'LIKE', `%${name}%`)
+      query.orWhere('surname', 'LIKE', `${surname}`)
+      query.orWhere('email', 'LIKE', `${email}`)
+    }
+
+    const { page, limit } = pagination
+    const users = await query.paginate(page, limit);
+
+    return response.send(users);
   }
 
   /**
@@ -40,7 +46,22 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response }) {
+    try {
+      const { name, surname, password, image_id, email } = request.all()
+      if (!name) return response.status(400).send({ error: 'Name not specified' })
+      if (!surname) return response.status(400).send({ error: 'Surname not specified' })
+      if (!password) return response.status(400).send({ error: 'Password not specified' })
+      if (!email) return response.status(400).send({ error: 'Email not specified' })
+
+      const user = await Users.create({ name, surname, password, email, image_id })
+      return response.status(201).send(user)
+    } catch (error) {
+      return response.status(400).send({
+        error: 'Couldn\'t create user.'
+      })
+
+    }
   }
 
   /**
@@ -52,20 +73,11 @@ class UserController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show({ params: { id }, response }) {
+    const user = await Users.findOrFail(id)
+    return response.send(user)
   }
 
-  /**
-   * Render a form to update an existing user.
-   * GET users/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
 
   /**
    * Update user details.
@@ -75,7 +87,16 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params: { id }, request, response }) {
+    const user = await Users.findOrFail(id)
+    try {
+      const { name, surname, password, image_id, email } = request.all();
+      user.merge({ name, surname, password, image_id, email })
+      await user.save()
+      return response.send(user)
+    } catch (error) {
+
+    }
   }
 
   /**
@@ -86,7 +107,10 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params:{id}, request, response }) {
+    const user = await Users.findOrFail(id)
+    user.delete()
+    return response.status(204).send()
   }
 }
 
